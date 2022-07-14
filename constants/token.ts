5a087350891473671b3cae8e08294e9e4f490cc9
Dominik Spitzli <dominik@spitzli.dev>
2022-07-14 23:23:09 +0200

    random secret

--- constants/token.ts
+++ constants/token.ts
@@ -1,10 +1,8 @@
 import { configs } from "config";
 import { createFernet, createSecret } from "fernet";
 
-export const Client = createFernet("7I5rwYZbBSevxuzZhtfjKcSJq8aMGVS3o_4tzgQswR4=");
+export const Client = createFernet(createSecret(32));
 
 export const Application = createFernet(createSecret(32));
 
 export const Bearer = createFernet(createSecret(32));
-
-export const Refresh = createFernet("7I5rwYZbBSevxuzZhtfjKcSJq8aMGVS3o_4tzgQswR4=");
